2147033883a3a354804edef60c8effdc818081a7
ry <atpapers@gmail.com>
2016-09-11 10:45:31 +0700

    Web view

--- app/src/main/java/com/thailand/sorayanakthon/trafficlearning/MainActivity.java
+++ app/src/main/java/com/thailand/sorayanakthon/trafficlearning/MainActivity.java
@@ -1,13 +1,50 @@
 package com.thailand.sorayanakthon.trafficlearning;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private ListView listView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.listView);
+        button = (Button) findViewById(R.id.button);
+
+
+        //Buttom Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.phonton1);
+                mediaPlayer.start();
+
+                //Show Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/XwekpIhm5c4"));
+                startActivity(intent);
+
+
+
+            }//onClick
+        });
+
+
+    }//Main Class
+
+} //Main Class
